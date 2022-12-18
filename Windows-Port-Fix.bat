f51b243f3c3496a56c101739510b60ee100941e2
Amit Parmar <amitparmar901@gmail.com>
2022-12-18 11:48:32 +0530

    initial commimt
    Fixes port related issues. If you ever face 'port is already in use' like errors, can use this.

---
+++ Windows-Port-Fix.bat
@@ -0,0 +1,8 @@
+::https://github.com/AmitxParmar/Windows-Port-Issue-Fix.git
+
+
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+net stop winnat
+net start winnat
+exit
